8d0a6ebee3f0add072f7e82c957d81f608666792
Anand Ramakrishna <theanandramakrishna@gmail.com>
2019-12-29 11:52:26 -0800

    basic electron app

---
+++ index.js
@@ -0,0 +1,26 @@
+const electron = require("electron");
+
+var win;
+
+function createWindow() {
+    if (win != null)
+    {
+        return;
+    }
+
+    win = new electron.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences : {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile("index.html");
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+electron.app.on("ready", createWindow);
+electron.app.on("activate", createWindow);
