3a34a423c27bf6ff21467fb9f022801e75920411
Anand Ramakrishna <theanandramakrishna@gmail.com>
2019-12-31 17:39:40 -0800

    basic vue working

--- view.js
+++ view.js
@@ -1,9 +1,27 @@
-alert('ok');
-$(function () { alert('ready') });
-/*
-$(document).ready(function() {
-    $("#startbtn").click(function() {
-        alert('ok');
-    });
+var word = new Vue({
+    el: '#word',
+    data: {
+      message: 'Word will come here'
+    }
 });
-*/
+
+var wordperms = new Vue({
+    el: '#wordperms',
+    data: {
+      message: 'Word perms will come here'
+    }
+});
+
+var start = new Vue({
+    el: '#startbtn',
+    methods: {
+        startGame: function() {
+            alert('Start the game');
+        }
+    }
+});
+
+
+
+
+
