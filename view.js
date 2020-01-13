36cc1f07821253b8cfb68c267c75339f059bfadc
theanandramakrishna <theanandramakrishna@gmail.com>
2020-01-12 21:05:21 -0800

    Gamestatus (#8)
    * state
    
    * state fix
    
    * timer display
    
    * state working
    
    * fix tests

--- view.js
+++ view.js
@@ -1,20 +1,23 @@
 const electron = require("electron");
 const game = electron.remote.require("./game");
 
+const DEFAULT_WORD = "empty";
+const DEFAULT_PERMS = [ 
+  { text: 'perm1' },
+  { text: 'perm2' }
+];
+
 var word = new Vue({
     el: '#word',
     data: {
-      message: 'word'
+      message: DEFAULT_WORD
     }
 });
 
 var wordperms = new Vue({
     el: '#wordperms',
     data: {
-      perms: [
-          { text: 'perm1' },
-          { text: 'perm2' }
-      ]
+      perms: DEFAULT_PERMS
     }
 });
 
@@ -27,18 +30,62 @@ var timeremaining = new Vue({
 
 var start = new Vue({
     el: '#startbtn',
+    data: {
+        isStopped: !isGameRunning()
+    },
     methods: {
         startGame: function() {
             game.start();
-            var baseword = game.getBaseword();
-            word.message = baseword.word;
-            wordperms.perms = new Array();
-            for (var i = 0; i < baseword.perms.length; i++) {
-                wordperms.perms.push({ text: baseword.perms[i]});
-            }
+            updateState();
         }
     }
 });
+
+var stop = new Vue({
+    el: '#stopbtn',
+    data: {
+        isRunning: isGameRunning()
+    },
+    methods: {
+        stopGame: function() {
+            game.reset();
+            updateState();
+        }
+    }
+});
+
+var gamestate;
+
+function isGameRunning() {
+    if (gamestate != null && gamestate.state == "running") {
+        return true;
+    }
+    return false;
+}
+
+function updateState() {
+    gamestate = game.getGameState();
+    start.isStopped = !isGameRunning();
+    stop.isRunning = isGameRunning();
+
+    if (timeremaining.timeremaining != gamestate.timeremaining) {
+        timeremaining.timeremaining = gamestate.timeremaining;
+    }
+    
+    if (!gamestate.baseword) {
+        word.message = DEFAULT_WORD;
+        wordperms.perms = DEFAULT_PERMS;
+    }
+    else if (word.message != gamestate.baseword.word) {
+        word.message = gamestate.baseword.word;
+        wordperms.perms = new Array();
+        for (var i = 0; i < gamestate.baseword.perms.length; i++) {
+            wordperms.perms.push({ text: gamestate.baseword.perms[i]});
+        }
+    }
+}
+updateState();
+setInterval(updateState, 500);
 
 
 
